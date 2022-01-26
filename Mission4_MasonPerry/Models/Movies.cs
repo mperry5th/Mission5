87b7dac1816bb4822300d514d153e944d2cf10f2
mperry5th <90285075+mperry5th@users.noreply.github.com>
2022-01-25 20:37:57 -0700

    Add project files.

---
+++ Mission4_MasonPerry/Models/Movies.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_MasonPerry.Models
+{
+    public class Movies
+    {
+        [Key]
+        [Required]
+        public int MovieId { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Director { get; set; }
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string Lent { get; set; }
+        public string Notes { get; set; }
+    }
+}
+
+Category Title	Year	Director	Rating	Edited	Lent To:	Notes
